e158381d550a5e35306830ec276561c33044594e
Daniel C <bitclaw@gmail.com>
2020-03-28 11:32:16 -0500

    chmod +x bash scripts and install vim in back container

--- app/data/init.sh
+++ app/data/init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo -e "\e[36mRunning database restore. Please wait for this process to finish..."
-mysql -h 0.0.0.0 -u root -proot -e < mydatabase.sql
+mysql -h 0.0.0.0 -u root -proot -e "CREATE DATABASE IF NOT EXISTS mydatabase CHARACTER SET utf8 COLLATE utf8_general_ci"
+mysql -h 0.0.0.0 -u root -proot -e mydatabase < mydatabase.sql
 echo -e "\e[32mDatabase restore finished!"
